c85e5cc885e2ee409fee734e6eb8436c4328489d
mzsl <95681537+anomalili@users.noreply.github.com>
2022-03-01 12:25:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+console.log("srciptben");
+
+
+var ertek;
+ertek = "szöveg";
+var a = "4";
+var b = 5;
+
+var osszeg = Number(a) + Number(b);
+
+console.log(osszeg);
+
+var logikai = false;
+
+if (logikai) {
+    console.log("igaz");
+} else { console.log("hamis"); }
+
+
+var szin = "red";
+
+if (szin === "red") {
+    console.log("piros")
+};
+if (szin === "orange") {
+    console.log("nagyarcs")
+};
+if (szin === "blue") {
+    console.log("kek")
+};
+if (szin === "purple") {
+    console.log("lila")
+};
+if (szin === "green") {
+    console.log("zold")
+};
+
+
+var szam = 2;
+
+if (szam === 2) {
+    console.log("ez szám")
+    console.log(typeof szam)
+}
+else {console.log("ez nem szám")}
---
+++ script2.js
@@ -0,0 +1,43 @@
+var tomb1 = [];
+var tomb2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var tomb3 = [5];
+console.log(tomb1.length);
+console.log(tomb2.length);
+console.log(tomb3.length);
+console.log(tomb3[0]);
+
+var szavak = ["alma", "bokréta", "virsli", "szalamandra", "nukemissile"];
+console.log(szavak);
+
+for (let index = 0; index < szavak.length; index++) { console.log(szavak[index]); }
+
+var gyumolcsokszine = ["piros", "zöld", "virsliszínű", "foltos", "piros"];
+
+var gyumolcsAr = [200, 1000, 230, 10000, 8];
+
+var vane = index < gyumolcsokszine.length;
+var index = 0;
+while (index < gyumolcsokszine.length && !(gyumolcsokszine[index] === "zold"))
+{index++; console.log(vane); }
+
+var i = 0;
+ertek = 0;
+
+for ( i < gyumolcsAr.length);
+{ertek += gyumolcsAr[i]
+i++;}
+console.log(ertek)
+
+var indi = 0;
+var pirosdb = 0;
+while (indi < gyumolcsokszine.length) {if ( gyumolcsokszine[indi] === "piros"); { pirosdb++; indi++;}
+else {indi++};
+
+var ar = 500;
+var draga = 0;
+var yx = 0;
+
+
+if (gyumolcsAr[yx] > ar){draga++;yx++;}
+else(yx++;)
+console.log(draga);
